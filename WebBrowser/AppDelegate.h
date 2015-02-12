98bcbae6c85c3b746dd5ffb77cdde5eadd4314f3
Carper <imc7@hl1-m60.labs.pitt.edu>
2015-02-12 17:06:57 -0500

    Initial Commit

---
+++ WebBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebBrowser
+//
+//  Created by Carper, Iyeshia Milan on 2/12/15.
+//  Copyright (c) 2015 Carper, Iyeshia Milan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebBrowser
+//
+//  Created by Carper, Iyeshia Milan on 2/12/15.
+//  Copyright (c) 2015 Carper, Iyeshia Milan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
